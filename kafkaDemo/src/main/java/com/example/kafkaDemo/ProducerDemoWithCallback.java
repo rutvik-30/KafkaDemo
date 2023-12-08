25dfe39ac2f6325b30a074a69d389d1f2f96cabd
Rutvik Choudhary <86093877+rutvik-30@users.noreply.github.com>
2023-12-08 16:07:30 +0530

    Update ProducerDemoWithCallback.java

--- kafkaDemo/src/main/java/com/example/kafkaDemo/ProducerDemoWithCallback.java
+++ kafkaDemo/src/main/java/com/example/kafkaDemo/ProducerDemoWithCallback.java
@@ -23,16 +23,6 @@ public class ProducerDemoWithCallback {
         // connect to Localhost
        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
-        // connect to Conduktor Playground
-		/*
-		 * properties.setProperty("bootstrap.servers",
-		 * "cluster.playground.cdkt.io:9092");
-		 * properties.setProperty("security.protocol", "SASL_SSL");
-		 * properties.setProperty("sasl.jaas.config",
-		 * "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";"
-		 * ); properties.setProperty("sasl.mechanism", "PLAIN");
-		 */
-
         // set producer properties
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
